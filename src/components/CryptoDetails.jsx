740939a76361511ac736a14e67a30d15b211c46a
Mukul Singhal <mukulsinghal98@gmail.com>
2021-10-02 15:47:15 +0530

    Added Loader

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import HTMLReactParser from "html-react-parser";
 import { useParams } from "react-router-dom";
 import millify from "millify";
-
-import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
-
 import { Col, Row, Typography, Select } from "antd";
 import {
   MoneyCollectOutlined,
@@ -18,42 +15,52 @@ import {
   ThunderboltOutlined,
 } from "@ant-design/icons";
 
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import Loader from "./Loader";
+import LineChart from "./LineChart";
+
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
   const { coinId } = useParams();
+  console.log(coinId);
   const [timeperiod, setTimeperiod] = useState("7d");
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-
+  const { data: coinHistory } = useGetCryptoHistoryQuery({
+    coinId,
+    timeperiod,
+  });
   const cryptoDetails = data?.data?.coin;
 
-  if (isFetching) return <div>Loading</div>;
+  if (!cryptoDetails) return <Loader />;
+  console.log(cryptoDetails);
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
   const stats = [
     {
       title: "Price to USD",
-      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
+      value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`,
       icon: <DollarCircleOutlined />,
     },
-    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    { title: "Rank", value: cryptoDetails.rank, icon: <NumberOutlined /> },
     {
       title: "24h Volume",
-      value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+      value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
       icon: <ThunderboltOutlined />,
     },
     {
       title: "Market Cap",
-      value: `$ ${
-        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
-      }`,
+      value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
       icon: <DollarCircleOutlined />,
     },
     {
       title: "All-time-high(daily avg.)",
-      value: `$ ${millify(cryptoDetails?.allTimeHigh.price)}`,
+      value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
       icon: <TrophyOutlined />,
     },
   ];
@@ -61,17 +68,17 @@ const CryptoDetails = () => {
   const genericStats = [
     {
       title: "Number Of Markets",
-      value: cryptoDetails?.numberOfMarkets,
+      value: cryptoDetails.numberOfMarkets,
       icon: <FundOutlined />,
     },
     {
       title: "Number Of Exchanges",
-      value: cryptoDetails?.numberOfExchanges,
+      value: cryptoDetails.numberOfExchanges,
       icon: <MoneyCollectOutlined />,
     },
     {
       title: "Aprroved Supply",
-      value: cryptoDetails?.approvedSupply ? (
+      value: cryptoDetails.approvedSupply ? (
         <CheckOutlined />
       ) : (
         <StopOutlined />
@@ -80,12 +87,12 @@ const CryptoDetails = () => {
     },
     {
       title: "Total Supply",
-      value: `$ ${millify(cryptoDetails?.totalSupply)}`,
+      value: `$ ${millify(cryptoDetails.totalSupply)}`,
       icon: <ExclamationCircleOutlined />,
     },
     {
       title: "Circulating Supply",
-      value: `$ ${millify(cryptoDetails?.circulatingSupply)}`,
+      value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
       icon: <ExclamationCircleOutlined />,
     },
   ];
@@ -111,7 +118,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* //LineChart */}
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
